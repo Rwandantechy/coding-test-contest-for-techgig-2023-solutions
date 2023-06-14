575fa4fd7b278c287e757f6bdea6c2572b22c0d9
Innocent Niyonzima <89442975+Rwandantechy@users.noreply.github.com>
2023-06-14 10:28:47 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,49 @@
+/* Read input from STDIN. Print your output to STDOUT*/
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <algorithm>
+#include <limits.h>
+#include <stdbool.h>
+
+int findMinimumEnergyLevel(const std::vector<int>& energyLevels, int capacity) {
+    std::vector<int> sortedEnergyLevels = energyLevels;
+    std::sort(sortedEnergyLevels.begin(), sortedEnergyLevels.end(), std::greater<int>());
+
+    int minEnergyLevel = 0;
+    int n = sortedEnergyLevels.size();
+
+    for (int i = 0; i < n; i++) {
+        int currentEnergyLevel = sortedEnergyLevels[i];
+        int count = std::count_if(sortedEnergyLevels.begin(), sortedEnergyLevels.begin() + i + 1,
+                                  [currentEnergyLevel](int energy) { return energy >= currentEnergyLevel; });
+
+        if (count >= capacity) {
+            minEnergyLevel = currentEnergyLevel;
+            break;
+        }
+    }
+
+    if (minEnergyLevel == 0)
+        return -1;  // Not possible to save exactly X animals
+    else
+        return minEnergyLevel;
+}
+
+int main(int argc, char *a[])
+{
+  std::vector<int> energyLevels = {1, 3, 2, 4, 5};
+    int capacity = 4;
+
+    int minimumEnergyLevel = findMinimumEnergyLevel(energyLevels, capacity);
+
+    if (minimumEnergyLevel == -1)
+        std::cout  << capacity <<std::endl;
+    else
+        std::cout << minimumEnergyLevel << std::endl;
+
+    return 0;
+}
